f22bb1c73a36e91056faad07559abc667d92adb3
Rempuia <rema.sailo@gmail.com>
2016-03-09 15:18:19 +0530

    content initial

---
+++ application/migrations/2016_03_08_110212_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+class Create_Users_Table {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('Users',function($table)
+                {
+                   $table->increments('id');
+                   $table->string('username');
+                   $table->string('password');
+                   $table->timestamps();
+                });
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('Users');
+	}
+
+}
